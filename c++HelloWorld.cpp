068190216a049e57e8247b2d1d0346410eea6849
magiccow444 <fortnitemaster619@gmail.com>
2024-08-20 02:14:56 -0700

    Did a few bro code C++ videos and got up to arithmetic operators

---
+++ c++HelloWorld.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+// #include <vector>
+
+// Comment
+/*
+Comment
+*/
+
+
+// THIS WAS FOR NAMESPACES
+// namespace first {
+//     int x = 1;
+// }
+// namespace second {
+//     int x = 2;
+// }
+
+
+// THIS IS FOR TYPEDEF
+// But it is basically just renaming data types
+// Also you usually add _t after for type
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+// typedef int num_t;
+// USING keyword is more popular and recommended nowadays cuz of something
+// using txt_t = std::string;
+// using num_t = int;
+
+int main() {
+    // ARITHMETIC OPERATORS: ------------------------------
+
+
+    return 0;
+}
+    // INTRO: --------------------------------
+
+    // std::cout << "I like pizza" << std::endl;
+    // std::cout << "It's really good" << '/n';
+
+    // int x = 5;
+    // int y = 6;
+    // int sum = x + y;
+
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+    // std::cout << sum << '\n';
+
+    // BASIC DATA TYPES: ------------------------
+
+    // int age = 21;
+    // int year = 2024;
+    
+    // double price = 10.99;
+    // double temp = 25.1;
+
+    // char grade = 'A';
+    // char initial = 'N';
+
+    // bool isStudent = true;
+    // bool isLame = false;
+
+    // std::string name = "Nico Mendoza";
+    // std::string day = "Wednesday";
+    // std::cout << "Hello " << name;
+
+    // CONSTANTS: --------------------------------
+
+    // Usually you make all letters uppercase in constants
+    // const double PI = 3.14159;
+    // const int LIGHT_SPEED = 299792458;
+
+    // NAMESPACES: ---------------------------------------
+
+    // using namespace first; 
+    // If this line below wasn't here the "using namespace" line would make x always refer to the "first" namespace
+    // int x = 0;
+
+    // std::cout << x << first::x << second::x;
+
+    // using namespace std;
+    // This is lowk an op namespace as it lets you write strings and cout's without the std, buuuut maybe don't use it as you might run into a naming conflict 
+
+    // cout << x;
+    // string name = "Nico";
+
+    // using  std::cout;
+    // using  std::string;
+    // This is what you should do instead of just using namespace std (namespace standard)
+
+    // TYPEDEFS: -----------------------------------------------
+
+    // pairlist_t pairlist;
+    // text_t firstName = "Nico";
+    // num_t age = 19;
+
+    // std::cout << firstName << '\n';
+    // std::cout << age << '\n';
+
+    
